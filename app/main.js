2a5803ea6d361df5ac6e8d9f1dbfb009705e3dda
hkkhchan <hkkhchan@gmail.com>
2016-11-16 00:30:03 -0500

    add Timer

--- app/main.js
+++ app/main.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Ken from './Ken.jsx';
+import Timer from './Timer.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Ken name='ken' />, document.getElementById('ken'));
+ReactDOM.render(<Timer />, document.getElementById('timer'));
 
