ff6dcc7c8508c5983f053886afbec72365695cfa
hkkhchan <hkkhchan@gmail.com>
2016-11-18 03:03:41 -0500

    add todoApp

--- app/main.js
+++ app/main.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Ken from './Ken.jsx';
 import Timer from './Timer.jsx';
+import TodoApp from './TodoApp.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Ken name='ken' />, document.getElementById('ken'));
 ReactDOM.render(<Timer />, document.getElementById('timer'));
+ReactDOM.render(<TodoApp />, document.getElementById('todoApp'));
 
